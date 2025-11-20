c7477cee868125158e00bd3437e3c1f24833d2c5
Rocks Batoon <rbatoon@bsnsports.com>
2025-11-20 15:41:25 -0800

    swtich from sqlite to postgres

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from "@/../generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma/client";
 
-export const db = new PrismaClient();
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export const db = new PrismaClient().$extends(withAccelerate());
