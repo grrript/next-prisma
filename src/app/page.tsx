4ff0518a56b34569de6df96772bc48d5150ca6e4
Rocks Batoon <rbatoon@bsnsports.com>
2025-11-21 00:12:24 -0800

    list snippets on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const render = snippets.map((item) => {
-    return <li key={item.id}>{item.title}</li>;
+    return (
+      <Link
+        key={item.id}
+        href={`snippets/${item.id}`}
+        className="border rounded p-4 flex justify-between items-center"
+      >
+        <div>{item.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
   return (
-    <div>
-      <h1>Home page</h1>
-      <ul>{render}</ul>
+    <div className="p-8">
+      <h1 className="my-2 text-xl font-bold">Home page</h1>
+      <div className="my-4 flex flex-col gap-4">{render}</div>
     </div>
   );
 }
