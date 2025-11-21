50dec4f4ce205821e275f5d209d7e67e2a332959
Rocks Batoon <rbatoon@bsnsports.com>
2025-11-20 17:25:12 -0800

    render list of snippets on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const render = snippets.map((item) => {
+    return <li key={item.id}>{item.title}</li>;
+  });
+
   return (
-    <div className="">
+    <div>
       <h1>Home page</h1>
+      <ul>{render}</ul>
     </div>
   );
 }
