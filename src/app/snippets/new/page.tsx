f2bfd5a93b3cb944151dd6b3703d39d2d1e37e86
Rocks Batoon <rbatoon@bsnsports.com>
2025-11-23 13:03:18 -0800

    add form validation

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,23 +1,30 @@
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+"use client";
+
+import * as actions from "@/snippet/actions";
+import { useActionState, startTransition } from "react";
 
 export default function AddSnippet() {
-  async function createSnippet(formData: FormData) {
-    "use server";
+  const [formState, action] = useActionState(actions.createSnippet, {
+    message: "",
+  });
 
-    const snippet = await db.snippet.create({
-      data: {
-        title: formData.get("title") as string,
-        code: formData.get("code") as string,
-      },
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    startTransition(() => {
+      action(formData);
     });
-    console.log(snippet);
-    await db.$disconnect();
-    redirect("/");
   }
+
   return (
     <div className="container mx-auto px-12">
-      <form action={createSnippet}>
+      {formState.message ? (
+        <div className="p-4 bg-red-200 border rounded border-red-400 text-red-700">
+          {formState.message}
+        </div>
+      ) : null}
+      <form onSubmit={handleSubmit}>
         <h3 className="py-10">Create a New Snippet</h3>
 
         <div className="flex gap-4 p-2">
--- src/snippet/actions/index.ts
+++ src/snippet/actions/index.ts
@@ -15,3 +15,28 @@ export const deleteSnippet = async (id: number) => {
   await db.snippet.delete({ where: { id } });
   redirect(`/`);
 };
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  const title = formData.get("title");
+  const code = formData.get("code");
+
+  if (typeof title !== "string" || title.length < 3) {
+    return { message: "Title must be longer." };
+  }
+
+  if (typeof code !== "string" || code.length < 8) {
+    return { message: "Code must be longer." };
+  }
+
+  await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+
+  redirect("/");
+}
